bf0ac9e202987a0470664bb5afc43bdf8bc29b5a
Steven Myers <50813742+Steven-Myers@users.noreply.github.com>
2022-06-26 15:23:57 -0500

    Add files via upload

---
+++ Week 7/greetings.js
@@ -0,0 +1,21 @@
+var greet = function greet(name) {
+  if (name === null || name === undefined) return 'Hello there!';
+  if (typeof name === typeof []) {
+    if (name.length > 0) {
+      let names = '';
+      name.forEach((elm) => {
+        names += ', ' + elm;
+      });
+      return 'Hello' + names;
+    }
+    return 'Hello there!';
+  }
+  if (name === name.toUpperCase()) {
+    return 'HELLO ' + name + '!';
+  }
+
+  return 'Hello, ' + name;
+}
+
+
+module.exports = greet;
